e06fd5fcac856931458ebfc057dea9fbeea1f315
Fabio D. Argañaraz A. <ing.fabio.arg@gmail.com>
2024-11-14 20:02:18 -0300

    Add files via upload

---
+++ Ejemplos en clase.py
@@ -0,0 +1,42 @@
+import math
+import random
+import os
+print('Hola Mundo')
+
+
+""" Bloque de comentarios
+de muchas 
+lineas """
+# Operaciones matemáticas
+print(5+6)
+print(6-5)
+print(6*5)
+print(2**3)
+print(6/5)
+print(10%8)
+# raiz de 25
+print(25**.5)
+print(math.sqrt(25))
+print(math.log10(100))
+# Aleatorios
+print(random.random())
+print(random.randint(1,3))
+
+#Ingreso de datos
+os.system("cls")
+""" nombre = input("Ingresa tu nombre: ")
+print("Hola, " + nombre + " !!")
+edad = int(input("Ingresa tu edad: "))
+print("Usted el año que viene tendrá " + str(edad+1) + " años")
+altura = float(input("Ingresa tu altura en metros: "))
+print("tu altura en centimetros es: " + str(altura*100) + " cm") """
+
+# Booleanos
+verdadero = True
+falso = False
+
+#Asignación multiple
+a, b, c = 2, 4, 6
+
+#comparación multiple
+print(a<b<c)
